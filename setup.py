a6644a9235f0a8a830af9b814a61ec60e905427d
Johannes Metzler <johannes.metzler@gmail.com>
2021-03-02 11:00:33 +0100

    add setup.py for python lib installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(name="nnlib",
+      version="0.1",
+      description="These are various functions and tools around deep learning with fastai, tensorrt, labeling and so one.",
+      author="Johannes Metzler",
+      author_email='johannes.metzler@gmail.com',
+      platforms=["Any"],
+      license="MIT",
+      url="https://github.com/johansi/nnlib",
+      packages=find_packages(),
+      )
