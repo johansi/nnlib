8a218f02acdb99953246552f2bbc9876eb15f1a7
Johannes Metzler <johannes.metzler@gmail.com>
2021-06-21 12:48:28 +0200

    bugfix batchsize estimation

--- nnlib/tools/pytorch_batchsize.py
+++ nnlib/tools/pytorch_batchsize.py
@@ -16,13 +16,16 @@ class Batch_Size_Estimator:
     def __loss_batch(self,batches):
         xdata = self.__xdata_orig.repeat(batches,1,1,1).to(self.__device)
         ydata = self.__ydata_orig.repeat(batches,1,1,1).to(self.__device)
+        mask = self.__mask.repeat(batches,1,1,1).to(self.__device)
+        hull = self.__hull.repeat(batches,1,1,1)
+        hull = hull[:,None].repeat(1,mask.shape[1],1,1).to(self.device)
         out = self.__net(xdata)
-        loss = self.__loss_func(out, ydata)
+        loss = self.__loss_func(out, ydata, mask, hull)
         loss.backward()
         self.__opt.step()
         self.__opt.zero_grad()
         loss_v = float(loss.detach().cpu())
-        max_mem = round(torch.cuda.max_memory_allocated(0)/1000/1000/1.049)
+        max_mem = round(torch.cuda.max_memory_allocated(self.__device)/1000/1000/1.049)
         del xdata
         del ydata
         del out
@@ -33,13 +36,15 @@ class Batch_Size_Estimator:
     
             
     def find_max_bs(self):
-        
+                
         mems_used = []
-        self.__xdata_orig,self.__ydata_orig = self.__dataset[0]
+        name, self.__xdata_orig,self.__ydata_orig, self.__mask, self.__hull = self.__dataset[0]
         for i in range(3):           
             mems_used.append(self.__loss_batch(i+1))
         mems_used = np.array(mems_used[1:])    
         mebi_per_set = mems_used[1]-mems_used[0]
-        mebi_per_set += mebi_per_set*0.07
-                    
-        return int(round((self.__gpu_info.total() -  self.__gpu_info.used())/mebi_per_set))
+        mebi_per_set += mebi_per_set*0.12
+                            
+        total = self.__gpu_info.total()
+        used = self.__gpu_info.used()        
+        return int(round((total -  used)/mebi_per_set))
